ea9a63e5ef27fc87c4fcd19d8273186d5b3235a2
tien.phan3 <tien.phan@weai.vn>
2022-11-26 15:37:03 +0700

    feat: Add login

--- hackathon/views/user.py
+++ hackathon/views/user.py
@@ -3,10 +3,10 @@ from rest_framework.viewsets import ModelViewSet
 from hackathon.models import UserProfile, User
 from django.http import JsonResponse
 from rest_framework.request import Request
-from rest_framework.response import Response
 
 from hackathon.serializers.user import UserProfileSerializer
 from rest_framework.decorators import action
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 
 class UserProfileViewSet(ModelViewSet):
     queryset = UserProfile.objects.all()
@@ -14,7 +14,6 @@ class UserProfileViewSet(ModelViewSet):
 
     @action(detail=False, methods=['post'])
     def signup(self, request: Request):
-        print(request.data)
         if request.method == "POST":
             username = request.data.get("username")
             password = request.data.get("password")
@@ -46,10 +45,61 @@ class UserProfileViewSet(ModelViewSet):
                         default_data=default_data,
                     )
                 else:
-                    return Response({"status": "fail", "message": "Invalid reference id"})
+                    return JsonResponse({"status": "fail", "message": "Invalid reference id"})
 
-                return Response({"status": "success"})
+                return JsonResponse({"status": "success"})
 
-            return Response({"status": "fail"})
+            return JsonResponse({"status": "fail"})
 
-        return Response({"status": "fail"})
+        return JsonResponse({"status": "fail"})
+
+    @action(detail=False, methods=['post'])
+    def login(self, request: Request):
+        if request.method == "POST":
+            username = request.data.get("username")
+            password = request.data.get("password")
+            username = User.objects.filter(username=username)
+            username = username[0].username if username else None
+            if username:
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    profile = UserProfile.objects.filter(user=user)
+                    if user.is_superuser:
+                        auth_login(request, user)
+                        if not profile:
+                            new_profile = UserProfile(
+                                user=user,
+                                fullname=user.get_full_name(),
+                                email=user.email,
+                            )
+                            new_profile.save()
+
+                        data = {
+                            "status": "success", 
+                            "message": "Login successful"
+                        }
+                        return JsonResponse(data)
+                    elif user.is_active and profile.is_active and profile.is_enable:
+                        auth_login(request, user)
+                        data = {
+                            "status": "success", 
+                            "message": "Login successful"
+                        }
+                        return JsonResponse(data)
+                    else:
+                        data = {
+                            "status": "fail", 
+                            "message": "Login failure, user is not active"
+                        }
+                        return JsonResponse(data)
+            else:
+                data = {
+                    "status": "fail", 
+                    "message": "Login failure"
+                }
+                return JsonResponse(data)
+        data = {
+            "status": "Fail",
+            "message": "Login failure"
+        }
+        return JsonResponse(data)
