67a5caa6067f88b2171cbf30ea4d311841cc274c
Phạm Tân Tị <kurokid0206@gmail.com>
2022-11-26 19:26:40 +0700

    feat: move template

--- green_recycle/urls.py
+++ green_recycle/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views, ajax
+from .views import index
 
 app_name = 'green_recycle'
 
 urlpatterns = [
-
+    path('', index.index, name='index'),
 ]
